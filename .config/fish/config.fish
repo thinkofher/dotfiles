2e971e17cde2b9a4db9fdc3c71413058adb2e2a1
thinkofher <mail@bdudek.xyz>
2021-08-02 20:34:10 +0200

    Update fish config.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -23,31 +23,4 @@ alias vim nvim
 alias mmosh "mosh main -- tmux a"
 alias gowtf "go clean -cache -modcache -i -r"
 
-# path
-# set -Ua fish_user_paths $HOME/.local/bin
-
-# go
-# set GOPATH $HOME/go
-# set GOROOT /usr/loca/go
-# set -Ua fish_user_paths $GOROOT/bin
-# set -Ua fish_user_paths $GOPATH/bin
-
-# npm
-# set -Ua fish_user_paths $HOME/.local/node_modules/bin
-
-# rust
-# set -Ua fish_user_paths $HOME/.cargo/bin
-
-# cabal
-# set -Ua fish_user_paths $HOME/.cabal/bin
-
-# pyenv
-# set PYENV_ROOT $HOME/.pyenv
-# set PATH $PYENV_ROOT/bin $PATH
-# status --is-interactive; and source (pyenv init -|psub)
-# status --is-interactive; and source (pyenv virtualenv-init -|psub)
-
-# fzf
-# set -Ua fish_user_paths $HOME/.fzf/bin
-
 source /opt/homebrew/opt/asdf/asdf.fish
