cccc702974197fb291e61eaa751e7242b9248cdf
thinkofher <mail@bdudek.xyz>
2021-08-22 16:09:30 +0200

    Use new block fish prompt.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,47 +1,31 @@
-# name: RobbyRussel
-#
-# You can override some default options in your config.fish:
-#   set -g theme_display_git_untracked no
-
-function _git_branch_name
-  echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
-end
-
-function _is_git_dirty
-  set -l show_untracked (git config --bool bash.showUntrackedFiles)
-  set untracked ''
-  if [ "$theme_display_git_untracked" = 'no' -o "$show_untracked" = 'false' ]
-    set untracked '--untracked-files=no'
-  end
-  echo (command git status -s --ignore-submodules=dirty $untracked 2> /dev/null)
-end
-
 function fish_prompt
-  set -l last_status $status
-  set -l cyan (set_color -o cyan)
-  set -l yellow (set_color -o yellow)
-  set -l red (set_color -o red)
-  set -l blue (set_color -o blue)
-  set -l green (set_color -o green)
-  set -l normal (set_color normal)
+    set -l last_status $status
+    set -l cwd (prompt_pwd)
 
-  if test $last_status = 0
-      set arrow "$green➜ "
-  else
-      set arrow "$red➜ "
-  end
-  set -l cwd $cyan(basename (prompt_pwd))
-
-  if [ (_git_branch_name) ]
-    set -l git_branch $red(_git_branch_name)
-    set git_info "$blue git:($git_branch$blue)"
-
-    if [ (_is_git_dirty) ]
-      set -l dirty "$yellow ✗"
-      set git_info "$git_info$dirty"
+    if not test $last_status -eq 0
+        set_color --bold white -b red
+        echo -n ' ! '
+        set_color normal
     end
-  end
 
-  echo -n -s $arrow ' ' $cwd $git_info $normal ' '
+    # Display current path
+    set_color black -b blue
+    echo -n " $cwd "
+
+    # Show git branch and dirty state
+    set -l git_branch (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
+    set -l git_dirty (command git status -s --ignore-submodules=dirty 2> /dev/null)
+    if test -n "$git_branch"
+        if test -n "$git_dirty"
+            set_color black -b yellow
+            echo -n " $git_branch "
+        else
+            set_color black -b green
+            echo -n " $git_branch "
+        end
+    end
+
+    # Add a space
+    set_color normal
+    echo -n ' '
 end
-
