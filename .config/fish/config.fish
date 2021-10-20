8ef3c36a995801bf2971c199ebad5b4c8dfdfe43
thinkofher <mail@bdudek.xyz>
2021-10-21 00:35:30 +0200

    feat: Remove fisher bootstrap script.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,16 +5,6 @@
 # startup of the shell. I am using fish as the interactive shell, at
 # top of the bash, so i don't have to hustle with all the ENV variables.
 
-
-# fisher bootstrap
-# uncomment it to install fisher (fish plugin manager) and the startup
-
-# if not functions -q fisher
-#     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-#     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-#     fish -c fisher
-# end
-
 # neovim aliases
 alias v "nvim -u ~/.config/nvim/clean.vim"
 alias e v
@@ -34,3 +24,7 @@ alias ct "cat $HOME/.config/git/commit-types"
 source /opt/homebrew/opt/asdf/asdf.fish
 
 fish_add_path /opt/homebrew/bin
+
+if test -e ~/.iterm2_shell_integration.fish
+    source ~/.iterm2_shell_integration.fish
+end
