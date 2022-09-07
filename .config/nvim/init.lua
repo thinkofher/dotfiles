131d75cbe61053f30ec6eb1754167768a0b26e95
thinkofher <mail@bdudek.xyz>
2022-09-07 14:05:51 +0200

    feat: defer loading plugins and lua config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,37 +1,40 @@
-local hotpot_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/hotpot.nvim"
-if vim.fn.empty(vim.fn.glob(hotpot_path)) > 0 then
-    print("Could not find hotpot.nvim, cloning new copy to", hotpot_path)
-    vim.fn.system({
-        "git",
-        "clone",
-        "https://github.com/rktjmp/hotpot.nvim",
-        hotpot_path,
-    })
-end
+vim.schedule(function()
+    local hotpot_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/hotpot.nvim"
+    if vim.fn.empty(vim.fn.glob(hotpot_path)) > 0 then
+        print("Could not find hotpot.nvim, cloning new copy to", hotpot_path)
+        vim.fn.system({
+            "git",
+            "clone",
+            "https://github.com/rktjmp/hotpot.nvim",
+            hotpot_path,
+        })
+    end
 
-local packer_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if vim.fn.empty(vim.fn.glob(packer_path)) > 0 then
-    print("Could not find packer.nvim, cloning new copy to", packer_path)
-    _G.packer_bootstrap = vim.fn.system({
-        "git",
-        "clone",
-        "--depth",
-        "1",
-        "https://github.com/wbthomason/packer.nvim",
-        packer_path,
-    })
-    vim.cmd [[packadd packer.nvim]]
-end
+    local packer_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+    if vim.fn.empty(vim.fn.glob(packer_path)) > 0 then
+        print("Could not find packer.nvim, cloning new copy to", packer_path)
+        _G.packer_bootstrap = vim.fn.system({
+            "git",
+            "clone",
+            "--depth",
+            "1",
+            "https://github.com/wbthomason/packer.nvim",
+            packer_path,
+        })
+        vim.cmd [[packadd packer.nvim]]
+    end
 
-require("hotpot").setup({
-    provide_require_fennel = true,
-    compiler = {
-        macros = {
-            env = "_COMPILER",
-            compilerEnv = _G,
-            allowedGlobals = false,
+    require("hotpot").setup({
+        provide_require_fennel = true,
+        compiler = {
+            macros = {
+                env = "_COMPILER",
+                compilerEnv = _G,
+                allowedGlobals = false,
+            },
         },
-    },
-})
+    })
 
-require('thinkofher.core')
+    require('thinkofher.core')
+    vim.cmd [[silent! edit!]]
+end)
